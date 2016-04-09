6a3ecbbc2264fb2f3a89ebb1412fe196867f5cd1
gullykhubi <gulshan.khubnani@yahoo.com>
2016-04-09 15:38:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var mongojs = require("mongojs");
  
 var ip_addr = '127.0.0.1';
-var port    =  '8123';
+var port    =  '8080';
  
 var server = restify.createServer({
     name : "myapp"
